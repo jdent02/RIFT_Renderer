c333b7b8e34a4cb6ad71fc48d6c0dbba6c178290
Jonathan Dent <jdent02@hotmail.com>
2019-03-15 21:01:19 -0500

    Add MIT license

--- src/core/pdfs/mixture_pdf.cpp
+++
@@ -1,1 +0,0 @@
-#include "mixture_pdf.h"
--- src/utility/containers/scene.h
+++ src/utility/containers/scene.h
@@ -22,26 +22,12 @@
 
 #pragma once
 
-#include "core/data_types/vec3.h"
-#include "itexture.h"
-#include "utility/noises/perlin.h"
+class icamera;
+class ihitable;
 
-class noise_texture : public itexture
+struct scene
 {
-  public:
-    noise_texture() = default;
-
-    explicit noise_texture(float sc)
-      : scale(sc){};
-
-    virtual vec3 value(float u, float v, const vec3& p) const override
-    {
-        //        return vec3(1.f, 1.f, 1.f) * 0.5f * (1 + noise.turb(scale *
-        //        p)); return vec3(1.f, 1.f, 1.f) * noise.turb(scale * p);
-        return vec3(1.f, 1.f, 1.f) * 0.5f *
-               (1 + sin(scale * p.z() + 10.f * noise.turb(p)));
-    }
-
-    perlin noise;
-    float  scale{1.f};
+    icamera*  cam;
+    ihitable* world;
+    ihitable* light_source;
 };
